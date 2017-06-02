f88763e25c2f065151828ecc6b993c18fc2f85a2
AshutoshKumar99 <ashutoshkumar101094@gmail.com>
2017-06-02 17:34:43 +0530

    Update sp2.java

--- sp2.java
+++ sp2.java
@@ -4,7 +4,7 @@ public static void main(String args[])
 {
 
 System.out.println("This is QA2 Side\tThank for using Jenkins.");
-System.out.println("My Laptop name is Dell Vostro 2510,and I have been using it since last 5 years,ThankYou.!")
+System.out.println("My Laptop name is Dell Vostro 2510,and I have been using it since last 5 years,ThankYou.!");
 System.out.println("Above statement is the statement after commit changes in the file sp2.java");
 
 
